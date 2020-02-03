b7c38f15ba28a329124917776e6ed7c8bc2bfa44
Frank Becker <fb@alien8.de>
2020-02-03 23:05:43 +0100

    Adding a simple CLI dispatcher
    Using docopt for help. Only module to jump into is poller.main()
    so far.
    
    Signed-off-by: Frank Becker <fb@alien8.de>

---
+++ src/discovergy/cli.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+""" Discovergy Data Analyzer
+Usage:
+   {cmd} <command> [<args>...]
+   {cmd} -h | --help | --version
+
+Commands:
+   poll       Poll data from the Discovergy endpoint
+
+Options:
+   -h, --help
+
+"""
+
+import sys
+
+from discovergy import __version__, poller
+
+from docopt import docopt
+
+
+def print_help() -> None:
+    """Print the help and exit."""
+    print("The sub command is unknown. Please try again.", end="\n\n")
+    print(__doc__.format(cmd=sys.argv[0]), file=sys.stderr)
+    sys.exit(1)
+
+
+def main():
+    """Parse arguments and dispatch to the submodule"""
+
+    dispatch = {
+        "poll": poller.main,
+    }
+
+    arguments = docopt(
+        __doc__.format(cmd=sys.argv[0]),
+        version=__version__,
+        options_first=True,
+    )
+
+    dispatch.get(arguments['<command>'], print_help)()
+
+
+if __name__ == '__main__':
+    main()
